37b208b63c3baa9afdb5df1a50851657870e5db2
bITeSys <80810717+bITeSys@users.noreply.github.com>
2021-03-17 22:42:08 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$pwd = "";
-$dbName = "treasurehunt";
-$user = "root";
+$pwd = "8d2d611f0f5075077857b68fb7042f8aa830966c060975783a6bd06fb4892804";
+$dbName = "d7uanuiosmq0is";
+$user = "lfkblfepuqflgr";
 ?>
