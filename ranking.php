941fde7232de2877fe9a1ed93489349ec6c451bd
bITeSys <80810717+bITeSys@users.noreply.github.com>
2021-03-18 09:59:21 +0530

    Update ranking.php

--- ranking.php
+++ ranking.php
@@ -39,9 +39,9 @@ $result4 = pg_execute($mysqli, "my_query4", array());
 //$query = "SELECT * FROM user_details ORDER BY u_lvl desc, time asc limit 10";
 
 if(pg_num_rows($result4)>0){
-    echo "<div style='text-align:-webkit-center;text-align:-moz-center'><table border='1px solid white' cellpadding='5px'><tr><td width='250px'>Name</td><td style='text-align:center; width:50px'>Level</td><td style='text-align:center; width:250px'>Time</td></tr>";
+    echo "<div style='text-align:-webkit-center;text-align:-moz-center'><table border='1px solid white' cellpadding='5px'><tr><td width='250px'>Name</td><td style='text-align:center; width:50px'>Level</td></tr>";
     while($row = pg_fetch_object($result4)){
-	echo "<tr><td>"$row->u_name"</td><td style='text-align:center'>"$row->u_lvl"</td><td style='text-align:center'>".date( "Y-M-d H:i:s", strtotime($row->time)+5.5*3600)."</td></tr>";
+	echo "<tr><td>"<?php echo $row->u_name ; ?>"</td><td style='text-align:center'>"<?php echo $row->u_lvl; ?>"</td></tr>";
     }
     echo "</table></div>";
 }
